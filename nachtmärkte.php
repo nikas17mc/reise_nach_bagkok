a42d7a89b068473b279be4681b4c2d006457e83e
tobiaswit06 <152172281+tobiaswit06@users.noreply.github.com>
2023-12-04 12:07:00 +0100

    Update nachtmärkte.php

--- nachtmärkte.php
+++ nachtmärkte.php
@@ -5,37 +5,34 @@ NACHTMÄRKTE CODE
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bangkok Nachtmärkte</title>
+    <title>Chatuchak Weekend Market</title>
 </head>
 <body>
 
 <?php
+// Informationen über den Chatuchak Weekend Market
+$marktName = "Chatuchak Weekend Market";
+$beschreibung = "Einer der größten Wochenendmärkte der Welt und ein absolutes Muss bei einem ersten Bangkok Besuch.";
 
-// Funktion zur Ausgabe von Informationen zu Nachtmärkten
-function displayNightMarket($name, $description, $openingHours) {
-    echo "<h2>$name</h2>";
-    echo "<p>$description</p>";
-    echo "<p><strong>Öffnungszeiten:</strong> $openingHours</p>";
-}
+// Informationen zu den Öffnungszeiten
+$oeffnungszeitenWoche = "Mi-Do von 7-18 Uhr";
+$oeffnungszeitenWochenende = "Sa-So von 9-18 Uhr";
+$freitagNightMarket = "Der Chatuchak Friday Night Market ist am Freitag von 18 Uhr bis Mitternacht geöffnet.";
 
-// Chatuchak Friday Night Market Informationen
-$chatuchakFridayNightMarket = array(
-    'name' => 'Chatuchak Friday Night Market',
-    'description' => 'Der Chatuchak Friday Night Market ist am Freitag von 18 Uhr bis Mitternacht geöffnet.',
-    'openingHours' => 'Mi-Do: 7-18 Uhr, Sa-So: 9-18 Uhr'
-);
+// Informationen zum Spazieren
+$bummelnInfo = "Mittlerweile besuchen wir den Chatuchak Weekend Market gerne zum spazieren, zum richtigen Nachtmarkt wird der Chatuchak Market nämlich nur freitags, da hat er von 18 Uhr bis Mitternacht geöffnet. Mittwoch, Donnerstags und Samstags kannst du dich ab etwa 9 Uhr ins Shopping-Paradies begeben.";
 
-// Jodd Fairs Night Market Informationen
-$joddFairsNightMarket = array(
-    'name' => 'Jodd Fairs Night Market',
-    'description' => 'Der Jodd Fairs Nightmarket ist der wohl hippste und aktuell angesagteste Nachtmarkt Bangkoks. Auf dem Jodd Fairs Nightmarket findest du eine geniale Auswahl an leckeren Köstlichkeiten und ein modernes Ambiente.',
-    'openingHours' => 'Öffnungszeiten variieren. Bitte überprüfen Sie die aktuellen Zeiten vor dem Besuch.'
-);
+// Ausgabe der Informationen
+echo "<h1>$marktName</h1>";
+echo "<p>$beschreibung</p>";
 
-// Nachtmärkte anzeigen
-displayNightMarket($chatuchakFridayNightMarket['name'], $chatuchakFridayNightMarket['description'], $chatuchakFridayNightMarket['openingHours']);
-displayNightMarket($joddFairsNightMarket['name'], $joddFairsNightMarket['description'], $joddFairsNightMarket['openingHours']);
+echo "<h2>Öffnungszeiten:</h2>";
+echo "<p>$oeffnungszeitenWoche</p>";
+echo "<p>$oeffnungszeitenWochenende</p>";
+echo "<p>$freitagNightMarket</p>";
 
+echo "<h2>Bummeln:</h2>";
+echo "<p>$bummelnInfo</p>";
 ?>
 
 </body>
