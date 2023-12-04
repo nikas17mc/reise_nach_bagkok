9410a29b717676e3215e00fa010a377621c66fa6
tobiaswit06 <152172281+tobiaswit06@users.noreply.github.com>
2023-12-04 12:52:17 +0100

    Update and rename gefahren_in_bagkok.php to gefahren_in_bangkok.php

---
+++ gefahren_in_bangkok.php
@@ -0,0 +1,49 @@
+                                                                                
+GEFAHREN IN BANGKOK:
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gefahren in Bangkok</title>
+</head>
+<body>
+
+<?php
+// Informationen über die Gefahren in Bangkok
+$verkehrInfo = "Der Verkehr in Bangkok kann sehr chaotisch sein, besonders zu Stoßzeiten. Es gibt viele Motorräder, Tuk-Tuks, Autos und öffentliche Verkehrsmittel. Sei vorsichtig beim Überqueren von Straßen und nutze Fußgängerüberwege, wenn möglich.";
+
+$taschendiebstahlInfo = "In belebten Touristengebieten besteht die Gefahr von Taschendiebstählen. Halte deine Wertsachen sicher und achte besonders in überfüllten Bereichen auf deine Taschen.";
+
+$gesundheitsrisikenInfo = "In tropischen Regionen wie Bangkok gibt es bestimmte Gesundheitsrisiken, einschließlich Mückenübertragener Krankheiten wie Dengue-Fieber. Verwende Mückenschutzmittel und informiere dich über notwendige Impfungen vor deiner Reise.";
+
+$kulturelleSensibilitaetInfo = "Respektiere die lokale Kultur und die Sitten. Vermeide es, heilige Stätten ohne angemessene Kleidung zu betreten, und respektiere lokale Bräuche und Traditionen.";
+
+$natuerlicheKatastrophenInfo = "Thailand liegt in einer seismisch aktiven Zone, und es besteht ein geringes Risiko für Erdbeben. Es ist ratsam, sich über die lokalen Notfallverfahren zu informieren.";
+
+$luftqualitaetInfo = "In manchen Perioden kann die Luftqualität in Bangkok aufgrund von Smog und Luftverschmutzung beeinträchtigt sein. Menschen mit Atemproblemen sollten dies berücksichtigen.";
+
+// Ausgabe der Informationen
+echo "<h1>Gefahren in Bangkok</h1>";
+echo "<h2>Verkehr:</h2>";
+echo "<p>$verkehrInfo</p>";
+
+echo "<h2>Taschendiebstahl:</h2>";
+echo "<p>$taschendiebstahlInfo</p>";
+
+echo "<h2>Gesundheitsrisiken:</h2>";
+echo "<p>$gesundheitsrisikenInfo</p>";
+
+echo "<h2>Kulturelle Sensibilität:</h2>";
+echo "<p>$kulturelleSensibilitaetInfo</p>";
+
+echo "<h2>Natürliche Katastrophen:</h2>";
+echo "<p>$natuerlicheKatastrophenInfo</p>";
+
+echo "<h2>Luftqualität:</h2>";
+echo "<p>$luftqualitaetInfo</p>";
+?>
+
+</body>
+</html>
