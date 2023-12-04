d334fb25275d8b83a8ec30a90dbd91543ec00147
tobiaswit06 <152172281+tobiaswit06@users.noreply.github.com>
2023-12-04 12:01:06 +0100

    Update einkaufszentren.php

--- einkaufszentren.php
+++ einkaufszentren.php
@@ -1,2 +1,54 @@
 
 EINKAUFSZENTREN CODE(CENTRAL WORLD):
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CentralWorld</title>
+</head>
+<body>
+
+<?php
+// Informationen über CentralWorld
+$einkaufszentrumName = "CentralWorld";
+$standort = "Herz von Bangkoks Geschäftsviertel Ratchaprasong";
+$beschreibung = "Eines der größten Einkaufszentren in Südostasien";
+
+// Allgemeine Merkmale
+$einkaufen = "CentralWorld beherbergt eine breite Palette von Einzelhandelsgeschäften, darunter internationale und lokale Marken, Modeboutiquen, Elektronikgeschäfte und vieles mehr.";
+$unterhaltung = "Der Komplex bietet Unterhaltungsmöglichkeiten wie ein Kino, eine Bowlingbahn und andere Freizeiteinrichtungen.";
+$essenTrinken = "Zahlreiche Restaurants und Lebensmittelgeschäfte bieten eine Vielzahl von Gerichten für unterschiedliche Geschmäcker und Vorlieben an.";
+$veranstaltungen = "In CentralWorld finden häufig Veranstaltungen, Ausstellungen und festliche Feiern statt, vor allem während großer Feiertage und Festivals.";
+
+// Standortinformationen
+$beliebteSehenswuerdigkeiten = "In der Nähe beliebter Sehenswürdigkeiten wie dem Erawan-Schrein, dem Siam Paragon und dem MBK Center";
+
+// Ausgabe der Informationen
+echo "<h1>$einkaufszentrumName</h1>";
+echo "<p>Standort: $standort</p>";
+echo "<p>$beschreibung</p>";
+
+echo "<h2>Allgemeine Merkmale:</h2>";
+echo "<p>$einkaufen</p>";
+echo "<p>$unterhaltung</p>";
+echo "<p>$essenTrinken</p>";
+echo "<p>$veranstaltungen</p>";
+
+echo "<h2>Standortinformationen:</h2>";
+echo "<p>$beliebteSehenswuerdigkeiten</p>";
+?>
+
+</body>
+</html>
+
+EINLAUFSZENTREN CODE(MBK CENTER)
+
+
+
+
+
+
+
+
