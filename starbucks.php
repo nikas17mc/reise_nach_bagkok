da19baaa8bb6550fbbed04905158e0cfba078ecf
tobiaswit06 <152172281+tobiaswit06@users.noreply.github.com>
2023-12-04 11:55:54 +0100

    Update starbucks.php

--- starbucks.php
+++ starbucks.php
@@ -1,2 +1,31 @@
 STARBUCKS CODE:
 
+!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Starbucks Filiale</title>
+</head>
+<body>
+
+<?php
+// Informationen über die Starbucks-Filiale
+$filialeName = "Starbucks im The Davis Hotel";
+$standort = "The Davis Hotel, Soi Sukhumvit 24";
+$oeffnungszeiten = "Täglich (06:00 bis 22:00 Uhr)";
+
+// Informationen über das Design
+$architekturInspiration = "Alte englische Architektur";
+$innenraumDesign = "Geschwungene Wände und hohe weiße Säulen";
+
+// Ausgabe der Informationen
+echo "<h1>$filialeName</h1>";
+echo "<p>Standort: $standort</p>";
+echo "<p>Öffnungszeiten: $oeffnungszeiten</p>";
+echo "<p>Die imposante Fassade des Hotels ist von der $architekturInspiration inspiriert.</p>";
+echo "<p>$innenraumDesign</p>";
+?>
+
+</body>
+</html>
