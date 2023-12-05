8d0ce6d0ed805e91add561932ce147e87c04d7c1
tobiaswit06 <152172281+tobiaswit06@users.noreply.github.com>
2023-12-05 12:15:04 +0100

    Update religion_in_bangkok.php

--- religion_in_bangkok.php
+++ religion_in_bangkok.php
@@ -2,7 +2,7 @@
 RELIGION IN THAILAND CODE:
 
 <!DOCTYPE html>
-<html lang="de">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,4 +12,51 @@ RELIGION IN THAILAND CODE:
 
 <?php
 
+echo "<h1>RELIGION IN BANGKOK</h1>";
+
+echo "<p>In Bangkok und in Thailand insgesamt spielt die Religion eine bedeutende Rolle im täglichen Leben und prägt die Kultur des Landes.
+Die vorherrschende Religion in Thailand ist der Buddhismus, insbesondere die Theravada-Schule.</p>";
+
+echo "<h2>Buddhismus:</h2>";
+echo "<p><b>Bedeutung des Buddhismus:</b> Der Buddhismus ist die vorherrschende Religion in Thailand, 
+und die Mehrheit der Thailänder bekennt sich zum Theravada-Buddhismus. Diese Form des Buddhismus betont die Lehren des historischen Buddha Siddhartha Gautama.</p>";
+
+echo "<h2>Tempel(Wat):</h2>";
+echo "<p>Bangkok ist reich an buddhistischen Tempeln, die als 'Wat' bekannt sind. Jeder Tempel ist ein Ort der Anbetung und des spirituellen Lernens. 
+Wat Phra Kaew und Wat Arun sind zwei der bekanntesten Tempel in Bangkok.</p>";
+
+echo "<h2>Buddhistische Rituale:</h2>";
+echo "<p>Buddhistische Rituale und Zeremonien sind in Bangkok weit verbreitet. 
+Dazu gehören das Geben von Almosen an Mönche, das Beten, das Anzünden von Räucherstäbchen und das Darbringen von Opfergaben.</p>";
+
+echo "<h2>Hinduismus:</h2>";
+echo "<p>Erawan-Schrein: Der Erawan-Schrein, Heimat des viergesichtigen Buddhas, ist ein hinduistischer Schrein, der von vielen Menschen in Bangkok verehrt wird. 
+Hier finden regelmäßig hinduistische Rituale statt.</p>";
+
+echo "<h2>Respekt vor religiösen Stätten:</h2>";
+echo "<p><b>Kleiderordnung:</b> Wenn du Tempel oder religiöse Stätten besuchst, solltest du dich respektvoll kleiden. 
+Bedecke Schultern und Knie, und entferne Schuhe, bevor du einen Tempel betrittst.</p>";
+
+echo "<h2>Verhalten:</h2>";
+echo "<p>Sei respektvoll und ruhig in religiösen Stätten. Vermeide lautes Sprechen und respektiere die laufenden Zeremonien.</p>";
+
+echo "<h2>Andere Religionen:</h2>";
+echo "<p><b>Andere Glaubensrichtungen:</b> Neben dem Buddhismus und Hinduismus gibt es in Bangkok auch Anhänger anderer Religionen,
+darunter der Islam, das Christentum und der Sikhismus.</p>";
+
+echo "<h2>Religiöse Toleranz:</h2>";
+echo "<p>Thailand ist bekannt für seine religiöse Toleranz, und verschiedene Religionen leben oft friedlich nebeneinander.</p>";
+
+echo "<h2>Feiertage und Festivals:</h2>";
+echo "<p><b>Wichtige buddhistische Feste:</b> Songkran (thailändisches Neujahrsfest) und Loy Krathong (Lichterfest) sind wichtige buddhistische Feste in Bangkok. 
+Während dieser Zeiten finden religiöse Zeremonien und festliche Aktivitäten statt.</p>";
+
+echo "<h2>Religiöse Feiertage:</h2>";
+echo "<p>Respektiere die religiösen Feiertage, die oft mit Tempelbesuchen und speziellen Zeremonien verbunden sind.
+An solchen Tagen können einige Geschäfte und öffentliche Einrichtungen geschlossen sein.</p>";
+
+?>
+
+</body>
+</html>
   
