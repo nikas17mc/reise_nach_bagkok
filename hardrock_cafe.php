488a559f059b2871184b5a5627465b3a9b73cc45
tobiaswit06 <152172281+tobiaswit06@users.noreply.github.com>
2023-12-05 11:49:41 +0100

    Update hardrock_cafe.php

--- hardrock_cafe.php
+++ hardrock_cafe.php
@@ -2,12 +2,47 @@
 HARDROCK CAFE CODE:
 
 <!DOCTYPE html>
+<html lang="de">
 <head>
-<meta charset = "UTF-8">
-<meta name =
-<title>Hardrock Cafe</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hard Rock Cafe Bangkok</title>
+
 </head>
 <body>
-  
-<?php
 
+<h1>Hard Rock Cafe Bangkok</h1>
+
+<?php
+// Informationen über das Hard Rock Cafe Bangkok
+$eröffnungsdatum = "22. April 1991";
+$neugestaltungsjahr = 2023;
+$beschreibung = "Genießen Sie das pulsierende Herz von Ratchaprasong Bangkok, wenn wir Ihnen das neu gestaltete Hard Rock Cafe Bangkok vorstellen. 
+Eingebettet zwischen den berühmten Einkaufszentren, Luxushotels und der spirituellen Anziehungskraft des Erawan-Schreins - Heimat des viergesichtigen Buddhas. 
+In der Nähe der BTS Chit Lom Sky Train Station gelegen, sind wir leicht zu erreichen.";
+
+$zielbeschreibung = "Das Hard Rock Cafe Bangkok, das die Gastronomie- und Unterhaltungsszene der Stadt bereichert, 
+hebt sich durch seine ikonische Sammlung von Musikmemorabilien ab. 
+Mit Sitzplätzen für bis zu 130 Gäste und unserem Rock Shop®, in dem Sie Sammlerstücke zur Erinnerung an Ihren Besuch entdecken können, 
+bieten wir Ihnen ein unvergessliches Erlebnis.
+
+Entspannen Sie sich in unserer stilvollen Bar oder auf der Außenterrasse und beobachten Sie das Treiben der Welt. Tauchen Sie ein in die Live-Musik, 
+während Sie an unseren charakteristischen Cocktails nippen und lokale Köstlichkeiten probieren. 
+Genießen Sie die Harmonie von Musik und kulinarischen Köstlichkeiten, die das HRC zu einem der besten Restaurants und Unterhaltungsorte in der Region machen.";
+
+// Ausgabe der Informationen
+echo "<h2>Eröffnungsdatum:</h2>";
+echo "<p>$eröffnungsdatum</p>";
+
+echo "<h2>Neugestaltungsjahr:</h2>";
+echo "<p>$neugestaltungsjahr</p>";
+
+echo "<h2>Beschreibung:</h2>";
+echo "<p>$beschreibung</p>";
+
+echo "<h2>Ziel für Essen und Unterhaltung in Bangkok:</h2>";
+echo "<p>$zielbeschreibung</p>";
+?>
+
+</body>
+</html>
