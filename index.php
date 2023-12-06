1614b850ee79e006531bae3b3b5d75b0c4abcce9
にこらい <71525867+nikas17mc@users.noreply.github.com>
2023-12-06 18:27:16 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Bangkok</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/style/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="header_p">
+            <p>Die Reise nach Bangkok</p>
+        </div>
+    </header>
+    <div class="container">
+        <div class="left_container">
+            <div class="list_container">
+                <div class="list_item">
+                    <div class="box_left"><img src="" alt=""></div>
+                    <div class="box_right">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="list_item">
+                    <div class="box_left"><img src="" alt=""></div>
+                    <div class="box_right">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="list_item">
+                    <div class="box_left"><img src="" alt=""></div>
+                    <div class="box_right">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="list_item">
+                    <div class="box_left"><img src="" alt=""></div>
+                    <div class="box_right">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="list_item">
+                    <div class="box_left"><img src="" alt=""></div>
+                    <div class="box_right">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="list_item">
+                    <div class="box_left"><img src="" alt=""></div>
+                    <div class="box_right">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="list_item">
+                    <div class="box_left"><img src="" alt=""></div>
+                    <div class="box_right">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="list_item">
+                    <div class="box_left"><img src="" alt=""></div>
+                    <div class="box_right">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="list_item">
+                    <div class="box_left"><img src="" alt=""></div>
+                    <div class="box_right">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="list_item">
+                    <div class="box_left"><img src="" alt=""></div>
+                    <div class="box_right">
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="right_container"></div>
+    </div>
+</body>
+
+</html>
